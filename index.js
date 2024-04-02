118ed4ba203fba1f9c89cdaa00bcb49fc9d7e9c6
chanosksks <165632109+chanosksks@users.noreply.github.com>
2024-04-02 11:04:20 +0800

    Add files via upload

---
+++ index.js
@@ -0,0 +1,7 @@
+document.getElementById('startJourney').addEventListener('mouseover', function() {
+    this.classList.add('shake');
+});
+
+document.getElementById('startJourney').addEventListener('mouseout', function() {
+    this.classList.remove('shake');
+});
